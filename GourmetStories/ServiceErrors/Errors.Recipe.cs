a73769ef685594a5ea937ff218271deed0a31977
andreeabrindea <andreeabrindea61@gmail.com>
2025-06-11 16:48:31 +0300

    refactor: add more errors to User's errors list

--- GourmetStories/ServiceErrors/Errors.Recipe.cs
+++ GourmetStories/ServiceErrors/Errors.Recipe.cs
@@ -9,7 +9,7 @@ public static class Errors
         public static Error NotFound => Error.NotFound(
             code: "Recipe.NotFound",
             description: "Recipe not found");
-        
+
         public static Error InvalidAuthorNameFormat => Error.Validation(
             code: "Recipe.InvalidAuthorName",
             description: "Invalid Author Name");
@@ -20,5 +20,28 @@ public static class Errors
         public static Error NotFound => Error.NotFound(
             code: "User.NotFound",
             description: "User not found");
+
+        public static Error EmptyUsername => Error.Validation(
+            code: "User.EmptyUsername",
+            description: "Username cannot be empty"
+        );
+
+        public static Error UsernameAlreadyExists => Error.Conflict(
+        code: "User.UsernameAlreadyExists",
+        description: "Username already exists");
+
+        public static Error EmailAlreadyRegisterd => Error.Conflict(
+        code: "User.EmailAlreadyRegisterd",
+        description: "This email is already in use.");
+
+        public static Error InvalidEmail => Error.Validation(
+        code: "User.InvalidEmail",
+        description: "This email is invalid");
+
+        public static Error EmailNotFound => Error.Validation(
+            code: "User.EmailNotFound",
+            description: "This email is not registered."
+        );
+
     }
 }
